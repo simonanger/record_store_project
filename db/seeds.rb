8d9f7220161a862364e60020fc828c9d2cf49a6c
Simon Anger <simonanger89@gmail.com>
2017-08-27 13:05:37 +0100

    Added more artists to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,14 @@ Artist.delete_all
 artist1 = Artist.new({'name' => 'King Witch', 'logo' => 'logo.url'})
 artist2 = Artist.new({'name' => 'Dune', 'logo' => 'logo.url'})
 artist3 = Artist.new({'name' => 'Converge', 'logo' => 'logo.url'})
+artist4 = Artist.new({'name' => 'Ice Cube', 'logo' => 'logo.url'})
+artist5 = Artist.new({'name' => 'Pink Floyd', 'logo' => 'logo.url'})
 
 artist1.save
 artist2.save
 artist3.save
+artist4.save
+artist5.save
 
 album1 = Album.new({'title' => 'Shoulders of Giants',
   'artwork' => '/shoulders.jpeg',
@@ -55,6 +59,48 @@ album6 = Album.new({'title' => 'You Fail Me',
   'cost' => '7',
   'sale' => '15',
   'artist_id' => artist3.id})
+album7 = Album.new({'title' => "AmeriKKa's Most Wanted",
+  'artwork' => '/amw.jpeg',
+  'genre' => 'Rap',
+  'stock' => '10',
+  'cost' => '3',
+  'sale' => '10',
+  'artist_id' => artist4.id})
+album8 = Album.new({'title' => 'Death Certificate',
+  'artwork' => '/dc.jpeg',
+  'genre' => 'Rap',
+  'stock' => '7',
+  'cost' => '7',
+  'sale' => '15',
+  'artist_id' => artist4.id})
+album9 = Album.new({'title' => 'The Predator',
+  'artwork' => '/predator.jpg',
+  'genre' => 'Rap',
+  'stock' => '0',
+  'cost' => '5',
+  'sale' => '5',
+  'artist_id' => artist4.id})
+album10 = Album.new({'title' => 'Dark Side of the Moon',
+  'artwork' => '/dark_side.png',
+  'genre' => 'Rock',
+  'stock' => '20',
+  'cost' => '3',
+  'sale' => '9',
+  'artist_id' => artist5.id})
+album11 = Album.new({'title' => 'Animals',
+  'artwork' => '/animals.jpg',
+  'genre' => 'Rock',
+  'stock' => '3',
+  'cost' => '5',
+  'sale' => '7',
+  'artist_id' => artist5.id})
+album12 = Album.new({'title' => 'Wish You Were Here',
+  'artwork' => '/wywh.png',
+  'genre' => 'Rock',
+  'stock' => '1',
+  'cost' => '7',
+  'sale' => '9',
+  'artist_id' => artist5.id})
 
   album1.save
   album2.save
@@ -62,6 +108,12 @@ album6 = Album.new({'title' => 'You Fail Me',
   album4.save
   album5.save
   album6.save
+  album7.save
+  album8.save
+  album9.save
+  album10.save
+  album11.save
+  album12.save
 
 binding.pry
 nil
